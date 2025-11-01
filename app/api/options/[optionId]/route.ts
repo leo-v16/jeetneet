a723fd739c16388a79965b9cb954ed994298a9f6
leo-v16 <abhishekmazumder72@gmail.com>
2025-11-02 00:32:51 +0530

    Some Typescript Error Fix

--- app/api/options/[optionId]/route.ts
+++ app/api/options/[optionId]/route.ts
@@ -1,18 +1,20 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/db/prisma';
 
-export async function PUT(request: Request, { params }: { params: { optionId: string } }) {
+export async function PUT(request: Request, context: { params: { optionId: string } }) {
+  const { optionId } = context.params;
   const data = await request.json();
   const option = await prisma.examOption.update({
-    where: { id: parseInt(params.optionId, 10) },
+    where: { id: parseInt(optionId, 10) },
     data,
   });
   return NextResponse.json(option);
 }
 
-export async function DELETE(request: Request, { params }: { params: { optionId: string } }) {
+export async function DELETE(request: Request, context: { params: { optionId: string } }) {
+  const { optionId } = context.params;
   await prisma.examOption.delete({
-    where: { id: parseInt(params.optionId, 10) },
+    where: { id: parseInt(optionId, 10) },
   });
   return new Response(null, { status: 204 });
 }
